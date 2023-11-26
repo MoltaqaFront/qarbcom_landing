bd6235ddf0e2cc38b8d059b19c4a41d70a76e5a3
shadyaabed <shady@moltaqa.net>
2023-11-26 10:31:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# QarbCom_website
---
+++ locales/en-US.js
@@ -0,0 +1,65 @@
+export default {
+  lang: 'ltr',
+
+  nav: {
+    home: 'HOME',
+    about: 'ABOUT APP',
+    screen: 'APP SCREENS',
+    download: 'DOWNLOAD NOW',
+    contact: 'CONTACT US',
+    terms: 'TERMS & CONDITIONS',
+    policy: 'Privacy Policy',
+    backToHome: 'BACK TO HOME',
+    lang: 'ع',
+  },
+  registerLink: 'REGISTER AS DRIVER',
+  mainSlider: [
+    {
+      mainTitle: 'About QarbCom',
+      title: 'Providing a unique and innovative user experience for marine travelers',
+      desc: 'We are your QarbCom, we aim to make the experience of booking boats and yachts into something fun and easy. We have extensive experience in the shipbuilding and marine tourism industry, and we work hard to provide a modern and reliable platform that meets your needs.',
+    },
+  ],
+  feature: [
+    {
+      image: '1.svg',
+      title: "Easy and fast booking",
+      desc: 'Easy and fast booking of boats and yachts through the application, with multiple options for dates, times and routes.',
+    },
+    {
+      image: '2.svg',
+      title: "Comprehensive and clear information",
+      desc: "Comprehensive and clear information about each boat or yacht, including specifications, photos and available facilities.",
+    },
+    {
+      image: '3.svg',
+      title: "User rating system",
+      desc: 'A user rating system for boats and yachts that helps you make a decision that matches your needs and preferences.'
+    },
+  ],
+  about: {
+    mainTitle: 'QarbCom app is your ideal choice',
+    title: 'About QarbCom',
+    desc: "<p>A multi-service online store application that allows users to purchase products and services online and have them delivered to their address. The application also includes reservation options for restaurants and events, and facilitates the process of obtaining the required services in an easy and convenient way. Those who are looking for a modern and sophisticated shopping experience, as it provides many distinctive and convenient services that make online shopping easy and enjoyable. Therefore, the application can be downloaded easily and benefit from all the services available on it with comfort and confidence. Get the best online shopping experience with the Pring application. Enjoy competitive prices and high quality of the products you buy through the QarbCom app. Fast and reliable delivery for all your purchases, with secure and easy payment options</p>",
+  },
+  downloadText: "<p>You can download the QarbCom app now by visiting the App Store on your smartphone, searching for QarbCom, and then downloading the app on your smartphone.</p>",
+  isoLink: "",
+  androidLink: "",
+  address: 'KSA - Jeddah',
+  terms: {
+    title: 'Terms And Conditions',
+  },
+  policy: {
+    title: 'Privacy Policy',
+  },
+  copyWrite: '©2023 All Rights Reserved For Moltaqa Software House',
+
+  contact: {
+    title: 'Contact Us',
+    name: 'name',
+    email: 'e-mail',
+    phone: 'phone',
+    message: 'message',
+    send: 'send ',
+    },
+}
